da56fd5c624567dd05dfc9d459f93ce25d5bfbf8
Aakash Kumar <aakashkumar051901@gmail.com>
2023-04-27 21:15:07 +0530

    Banking assignment

--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -4,8 +4,36 @@ public class SavingsAccount extends BankAccount{
     double rate;
     double maxWithdrawalLimit;
 
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public double getMaxWithdrawalLimit() {
+        return maxWithdrawalLimit;
+    }
+
+    public void setMaxWithdrawalLimit(double maxWithdrawalLimit) {
+        this.maxWithdrawalLimit = maxWithdrawalLimit;
+    }
+
     public SavingsAccount(String name, double balance, double maxWithdrawalLimit, double rate) {
         // minimum balance is 0 by default
+
+        super(name, 0);
+        this.rate = rate;
+        this.maxWithdrawalLimit = maxWithdrawalLimit;
+
+    }
+    public SavingsAccount(String name, double maxWithdrawalLimit, double rate) {
+        // minimum balance is 0 by default
+
+        super(name, 0);
+        this.rate = rate;
+        this.maxWithdrawalLimit = maxWithdrawalLimit;
 
     }
     public void withdraw(double amount) throws Exception {
@@ -13,16 +41,38 @@ public class SavingsAccount extends BankAccount{
         // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
         // 2. "Insufficient Balance" : If the amount exceeds balance
 
+        if (amount > maxWithdrawalLimit) {
+            throw new Exception("Maximum Withdraw Limit Exceed");
+        }
+
+        if (amount > super.getBalance()) {
+            throw new Exception("Insufficient Balance");
+        }
+
+        super.setBalance(super.getBalance() - amount);
     }
 
     public double getSimpleInterest(int years){
         // Return the final amount considering that bank gives simple interest on current amount
 
+        /*
+        Simple Interest = (P x T x R)/100
+        Where,
+
+        P is the principal amount
+        T is the time and
+        R is the rate
+         */
+
+        return (double) (super.getBalance() * years * rate) / 100;
     }
 
     public double getCompoundInterest(int times, int years){
         // Return the final amount considering that bank gives compound interest on current amount given times per year
 
+        // Principle*(1+(rate / 100))^time – Principle
+
+        return (double) super.getBalance() * (Math.pow((1 + rate / 100), times))  - super.getBalance();
     }
 
 }
--- src/main/java/com/driver/StudentAccount.java
+++ src/main/java/com/driver/StudentAccount.java
@@ -4,8 +4,28 @@ public class StudentAccount extends BankAccount{
 
     String  institutionName;
 
+    public String getInstitutionName() {
+        return institutionName;
+    }
+
+    public void setInstitutionName(String institutionName) {
+        this.institutionName = institutionName;
+    }
+
+
     public StudentAccount(String name, double balance, String  institutionName) {
         //minimum balance is 0 by default
+
+        super(name, balance);
+        this.institutionName = institutionName;
+
+    }
+
+    public StudentAccount(String name, String  institutionName) {
+        //minimum balance is 0 by default
+
+        super(name, 0);
+        this.institutionName = institutionName;
 
     }
 
